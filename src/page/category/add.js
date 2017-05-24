cd724ef8c6cb12bdf97601da31c6a9d0f3cd0660
zhaoning <zhaoning_IT@163.com>
2017-05-24 19:05:13 +0800

    fix

--- src/page/category/add.js
+++ src/page/category/add.js
@@ -11,7 +11,7 @@ import {
     ScrollView,
     TouchableHighlight
 } from 'react-native';
-import ImagePicker from 'react-native-image-picker';
+import ImagePicker from 'react-native-image-crop-picker';
 
 import styles from './style.js';
 
@@ -20,7 +20,15 @@ export default class Add extends Component {
         title: '添加多肉',
     };
 
+    constructor(props) {
+        super(props);
+        this.state = {avatarSource: ''};
+    }
+
 	render() {
+        const avatarSource  = this.state.avatarSource;
+        // const avatarSource  = {uri: 'http://img.funshion.com/sdw?oid=d25899ae9c87dfbb6f1680fd861c25e7&w=400&h=225'};
+        console.log( avatarSource, '----' );
 		return (
             <View>
                 <View>
@@ -34,6 +42,9 @@ export default class Add extends Component {
                 <View>
                     <Text>封面：</Text>
                     <Button title="选择个封面" onPress={ () => this._onPosterPress() }/>
+                    {avatarSource.length > 0 && 
+                        <Image source={avatarSource} style={{width:100, height:100}}/>
+                    }
                 </View>
                 <View>
                     <Text>相册：</Text>
@@ -43,41 +54,14 @@ export default class Add extends Component {
 	}
 
     _onPosterPress() {
-        
-        var options = {
-            title: '请选择照片',
-            storageOptions: {
-                skipBackup: true,
-                path: 'images'
-            }
-        };
-
         /**
          * The first arg is the options object for customization (it can also be null or omitted for default options),
          * The second arg is the callback which sends object: response (more info below in README)
          */
-        ImagePicker.showImagePicker(options, (response) => {
-            console.log('Response = ', response);
-
-            if (response.didCancel) {
-                console.log('User cancelled image picker');
-            }
-            else if (response.error) {
-                console.log('ImagePicker Error: ', response.error);
-            }
-            else if (response.customButton) {
-                console.log('User tapped custom button: ', response.customButton);
-            }
-            else {
-                let source = { uri: response.uri };
-
-                // You can also display the image using data:
-                // let source = { uri: 'data:image/jpeg;base64,' + response.data };
-
-                this.setState({
-                    avatarSource: source
-                });
-            }
+        ImagePicker.openPicker({
+            multiple: true
+        }).then(images => {
+            console.log('images = ', images);
         });
     }
 }
